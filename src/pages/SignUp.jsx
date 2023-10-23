5165d599b411ceab9976b5366ce92affe9a45cc0
Kontohquaye <109652640+Kontohquaye@users.noreply.github.com>
2023-10-23 16:48:54 +0000

    add users route

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,17 +1,93 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+import { toast } from "react-toastify";
+
+// api
+import backendInstance from "../utils/api";
+
+// reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SIGNUP_FETCH":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { loading: false, error: "" };
+    case "FETCH_FAILED":
+      // console.log(action.payload);
+      return { loading: false, error: { ...action.payload } };
+    case "RESET":
+      // console.log(action.payload);
+      return { loading: false, error: "" };
+    default:
+      return { state };
+  }
+};
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const redirectUrl = new URLSearchParams(search).get("redirect");
+  const redirect = redirectUrl;
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [{ loading, error }, dispatch] = useReducer(reducer, {
+    loading: false,
+    error: "",
+  });
+
+  // checkuser
+  // doi!
+
+  // details
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  // signup handler
+  const handleSignUp = async (e) => {
+    setPasswordMatch(true);
+    e.preventDefault();
+    // signup fetch
+    if (password !== confirmPassword) {
+      setPasswordMatch(false);
+      dispatch({ type: "RESET" });
+    } else {
+      setPasswordMatch(true);
+      // fetch
+      try {
+        dispatch({ type: "SIGNUP_FETCH" });
+        const userInfo = await backendInstance.post(
+          "/api/users/signup",
+          {
+            username: userName.toLowerCase(),
+            email,
+            password,
+          },
+          { withCredentials: true }
+        );
+        dispatch({ type: "FETCH_SUCCESS" });
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        navigate(redirect ? redirect : "/");
+      } catch (error) {
+        const {
+          response: { data },
+        } = error;
+        // console.log(data);
+        dispatch({ type: "FETCH_FAILED", payload: data });
+        toast.error("signup failed");
+      }
+    }
+  };
   return (
     <div className="sign-in flex justify-center items-center min-h-[80vh] ">
       <div className="content  p-4 w-min flex flex-col gap-2 justify-center items-center ">
         <h1 className="font-bold text-2xl">Sign Up</h1>
         <form className="flex flex-col justify-center  ">
           {/* username */}
-          <div className="username flex flex-col gap-1 mb-4">
+          <div className="username flex flex-col gap-1 mb-1">
             <label htmlFor="username" className="font-semibold">
               Username :
             </label>
@@ -19,11 +95,20 @@ const SignUp = () => {
               type="username"
               name="username"
               id="username"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
               className="border-[1px] border-accent p-2 bg-white"
             />
+            {error && (
+              <p className="text-center text-secondary font-semibold">
+                {error.username}
+              </p>
+            )}
           </div>
           {/* email */}
-          <div className="email flex flex-col gap-1 mb-4">
+          <div className="email flex flex-col gap-1 mb-1">
             <label htmlFor="email" className="font-semibold">
               Email :
             </label>
@@ -31,8 +116,17 @@ const SignUp = () => {
               type="email"
               name="email"
               id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               className="border-[1px] border-accent p-2 bg-white"
             />
+            {error && (
+              <p className="text-center text-secondary font-semibold">
+                {error.email}
+              </p>
+            )}
           </div>
           {/* password */}
           <div className="password flex flex-col gap-1">
@@ -45,7 +139,11 @@ const SignUp = () => {
                 required
                 name="password"
                 id="password"
-                className="border-[1px] border-accent p-2  mb-4 w-full"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="border-[1px] border-accent p-2  mb-1 w-full"
               />
               {/* icon */}
               {showPassword ? (
@@ -63,6 +161,11 @@ const SignUp = () => {
                   }}
                 />
               )}
+              {error && (
+                <p className="text-center text-secondary font-semibold">
+                  {error.password}
+                </p>
+              )}
             </div>
           </div>
           {/* confirm password */}
@@ -76,7 +179,11 @@ const SignUp = () => {
                 required
                 name="confirmPassword"
                 id="confirmpassword"
-                className="border-[1px] border-accent p-2  mb-4 w-full"
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+                className="border-[1px] border-accent p-2  mb-1 w-full"
               />
               {/* icon */}
               {showConfirmPassword ? (
@@ -95,10 +202,20 @@ const SignUp = () => {
                 />
               )}
             </div>
+            {!passwordMatch && (
+              <p className="text-center text-secondary font-semibold mb-2">
+                password doesn't match
+              </p>
+            )}
           </div>
 
-          <button className="bg-primary-200 text-white font-semibold p-2 hover:opacity-70">
-            Sign Up
+          <button
+            onClick={(e) => {
+              handleSignUp(e);
+            }}
+            className="bg-primary-200 text-white font-semibold p-2 hover:opacity-70"
+          >
+            {loading ? "Signing up..." : "Sign Up"}
           </button>
           <div className="info font-light text-center">
             Have an account?
