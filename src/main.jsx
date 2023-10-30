fe900b8e71d3a624ccdde745140743b9f1c4b920
Kontohquaye <109652640+Kontohquaye@users.noreply.github.com>
2023-10-30 21:39:39 +0000

    disable dev tools on production

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 // Load the .env file
 // import dotenv from "dotenv";
 // dotenv.config();
@@ -10,6 +10,8 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import App from "./App.jsx";
 import "./index.css";
 import StoreProvider from "./services/Store.jsx";
+
+if (import.meta.env.VITE_NODE_ENV === "production") disableReactDevTools();
 
 const apiKey = import.meta.env.VITE_CLIENT_ID;
 ReactDOM.createRoot(document.getElementById("root")).render(
